53af1c4e1a3a60e2d4dd2b1197727443cd74ad69
ynojima <mail@ynojima.net>
2014-03-21 17:59:43 -0600

    Update hypervisor_capabilities record of KVM
    to indicate live storage migration support

--- setup/db/db/schema-430to440.sql
+++ setup/db/db/schema-430to440.sql
@@ -764,3 +764,5 @@ ALTER TABLE `cloud`.`networks` ADD COLUMN streched_l2 boolean default false;
 ALTER TABLE `cloud`.`vpc` ADD COLUMN region_level_vpc boolean default false;
 ALTER TABLE `cloud`.`load_balancer_vm_map` ADD COLUMN instance_ip VARCHAR(40);
 ALTER TABLE `cloud`.`load_balancer_vm_map` DROP KEY `load_balancer_id`, ADD UNIQUE KEY load_balancer_id (`load_balancer_id`, `instance_id`, `instance_ip`);
+
+UPDATE hypervisor_capabilities SET storage_motion_supported=1 WHERE hypervisor_type='KVM' AND hypervisor_version='default';
